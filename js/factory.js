7254d54cefdbe96a690f63cbef82fa74f7d2d250
nelumbo <remi.chambiet@laposte.net>
2022-04-13 14:17:18 +0200

    debut du mise en forme des cards

--- js/factory.js
+++ js/factory.js
@@ -4,7 +4,7 @@ function recipesFactory(data) {
       var ingredienthtml='';
         data.ingredients.forEach((ingredients) => {
 
-            ingredienthtml=ingredienthtml+`<li class="list-group-item ingredient">${ingredients.ingredient}</li><p>${ingredients.quantity}${ingredients.unit}</p>`
+            ingredienthtml=ingredienthtml+`<li class="list-group-item ingredient">${ingredients.ingredient} : ${ingredients.quantity}${ingredients.unit}</li>`
             
         });
         data.ustensils.forEach((ustensils)=>{
@@ -17,15 +17,14 @@ function recipesFactory(data) {
 
 
 
-        cardshtml=`<div class="col" id="${data.id}">
-        <div class="card">
+        cardshtml=`<div class="col " id="${data.id}">
+        <div class="card ">
           <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body" id="${data.id}">
+          <div class="card-body row row-cols-2" id="${data.id}">
             <h5 class="card-title">${data.name}</h5>
             <ul class="list-group">
             ${ingredienthtml}
             </ul>
-              ${data.time} min</p>
               <p class="card-text">
               ${data.description}</p>
           </div>
